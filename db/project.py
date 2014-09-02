a8a744763106dcf121987fbf0dc875104f0231b7
Robert M. Johnson <rjohnson0186@gmail.com>
2014-09-02 10:27:34 -0400

    Added license information

--- db/project.py
+++ db/project.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
-
 '''
-    Author:       Robert M. Johnson <rmj49@georgetown.edu>
-    Organization: ICBI, Georgetown University <http://icbi.georgetown.edu>
+    medTurk (inspired by Amazon's Mechanical Turk) supports clinical research by using the 
+    ingenuity of humans to convert unstructured clinical notes into structured data.
 
+    Copyright (C) 2014 Innovation Center for Biomedical Informatics (ICBI)
+                       Georgetown University <http://icbi.georgetown.edu/>
+    
+    Author: Robert M. Johnson "matt"
+            <rmj49@georgetown.edu>
+            <http://mattshomepage.com/>
+            <@mattshomepage>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
-
 from medturk.db import db
-
-
 
 
 def save(name):
