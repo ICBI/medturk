e0b6e2b713a5de1748056a1e753a976297755c68
Robert M. Johnson <rjohnson0186@gmail.com>
2014-10-07 12:16:22 -0400

    Updated file

--- db/project.py
+++ db/project.py
@@ -26,10 +26,6 @@
 '''
 from medturk.db import db
 from bson import ObjectId
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.MIMEImage import MIMEImage
 
 
 def get(_id):
@@ -42,66 +38,8 @@ def add_analyst(project_id, email):
 
     # Add analyst to project
     p = get(project_id)
-
-
-    name        = p['name']
-    description = p['description']
-    p['analysts'] = [email]
+    p['analysts'] = list(set(p.get('analysts', []) + [email]))
     db.projects.save(p)
-
-    # Now email them
-
-
-    # Create a text/plain message
-    me       = 'rjohnson0186@gmail.com'
-    you      = email
-    username = 'rjohnson0186@gmail.com'
-    password = ''
-
-
-    # Create message container - the correct MIME type is multipart/alternative.
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = 'medTurk'
-    msgRoot['From'] = me
-    msgRoot['To'] = you
-
-
-    # Record the MIME types of both parts - text/plain and text/html.
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
-
-
-    msgText = MIMEText('This is the alternative plain text message.')
-    msgAlternative.attach(msgText)
-
-    # We reference the image in the IMG SRC attribute by the ID we give it below
-    #msgText = MIMEText('<img src="cid:image1" /> You\'ve been invited to participate in the ' + name + ' project on medTurk. ' + description +'. By answering questions, you are helping to drive clinical research forward. <a href="http://127.0.0.1:5000/medturk/login.html">Click here</a> to get started, and good luck!', 'html')
-    
-    msgText = MIMEText('You\'ve been invited to participate in the ' + name + ' project on medTurk. ' + description +'. By answering questions, you are helping to drive clinical research forward. <a href="http://127.0.0.1:5000/medturk/login.html">Click here</a> to get started, and good luck!', 'html')
-    msgAlternative.attach(msgText)
-
-    # This example assumes the image is in the current directory
-    #fp = open('/Users/matt/Desktop/medkit.png', 'rb')
-    #msgImage = MIMEImage(fp.read())
-    #fp.close()
-
-    # Define the image's ID as referenced above
-    #msgImage.add_header('Content-ID', '<image1>')
-    #msgRoot.attach(msgImage)
-
-
-
-
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
-    server = smtplib.SMTP("smtp.gmail.com:587")
-    server.starttls()
-    server.login(username, password)
-    server.sendmail(me, you, msgRoot.as_string())
-    server.quit()
-
-
-
 
 def save(_id, name, description, dataset_id, model_id):
     '''
