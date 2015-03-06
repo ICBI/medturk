02322480686621699a8bc5455e3d93bda0b1208c
Robert M. Johnson <rjohnson0186@gmail.com>
2015-03-06 13:13:22 -0500

    Updated config file

--- node_js/config.js
+++ node_js/config.js
@@ -4,7 +4,10 @@ module.exports = {
     'api_url'       : 'https://localhost',
     'ui_path'       : '../ui',
     'secret_key'    : 'replace me with a secret key',
-    'datasets_path' : '/Users/matt/Development/git/ICBI/medturk/datasets/',
+    'datasets_path' : '/medturk/datasets/',
     'cert_path' 	: './security/medturk-cert.pem',
-    'key_path' 		: './security/medturk-key.pem'
+    'key_path' 		: './security/medturk-key.pem',
+    'ldap_url'      : 'ldaps://directory.placeholder.edu',
+    'ldap_dn'       : 'ou=People,dc=placeholder,dc=edu',
+    'use_ldap'		: false
 }
