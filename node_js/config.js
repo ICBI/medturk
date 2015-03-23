d6f9b2edfc5f3e5b587504d98c57b4eadc523b00
Robert M. Johnson <rjohnson0186@gmail.com>
2015-03-23 10:02:27 -0400

    Updated config file

--- node_js/config.js
+++ node_js/config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     'db_url'        : 'mongodb://localhost:27017/medturk',
-    'api_url'       : 'https://10.129.21.4',
+    'api_url'       : 'https://localhost',
     'ui_path'       : '../ui',
     'secret_key'    : 'replace me with a secret key',
-    'datasets_path' : '/Users/matt/Development/git/ICBI/medturk/datasets/',
+    'datasets_path' : '../datasets/',
     'cert_path'     : './security/medturk-cert.pem',
     'key_path'      : './security/medturk-key.pem',
     'ldap_url'      : 'ldaps://directory.georgetown.edu',
